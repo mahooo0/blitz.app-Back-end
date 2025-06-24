0e5fe8624ed6c0d8746de05750430a0a35bb84bc
mahooo <sevinmuhammed06@gmail.com>
2025-06-24 21:46:13 +0400

    deploy 2

--- index.js
+++ index.js
@@ -4,10 +4,16 @@ const ConnectToMangoose = require('./utils/connectMangoose');
 const ListsRouter = require('./api/list/router');
 const TusksRouter = require('./api/tusks/router');
 const app = express();
+const cors = require('cors');
+
 app.use(express.json());
+app.use(cors());
 
 // Connect to MongoDB
 ConnectToMangoose();
+app.get('/', (req, res) => {
+    res.send('Welcome to the Task Management API');
+});
 app.use('/auth', AuthRouter);
 app.use('/lists', ListsRouter);
 app.use('/tuks', TusksRouter);
